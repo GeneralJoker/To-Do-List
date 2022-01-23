54a685bcb8e983cc7f1a308edbde122710f42908
GreatGeneral <hamzamohamed113@gmail.com>
2022-01-23 22:24:29 +0000

    Update script.js
    Made an option to create a new list by pressing the enter key as well.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ $(document).ready(function () {
         $('<li class="list-group-item"><span class="remove">X</span>' + $('#input').val() + '</li>').appendTo('#list');
         $('#input').val(null);
     });
+    
+    //Key pressed event for the enter key
+    $('#input').bind("enterKey", function (e) {
+        $('<li class="list-group-item"><span class="remove">X</span>' + $('#input').val() + '</li>').appendTo('#list');
+        $('#input').val(null);
+    });
 
+    $('#input').keyup(function (e) {
+        if (e.keyCode == 13) {
+            $(this).trigger("enterKey");
+        }
+    });
+    
 
 });
