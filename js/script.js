f7340c0b72f5c4c751767ac3d4015db6bbbad41a
GreatGeneral <hamzamohamed113@gmail.com>
2022-01-25 23:58:59 +0000

    Bootsrap CDN

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     //Click Event
     $('#addButton').click(function () {
-        $('<li><input type="text" class="text" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span></li>').appendTo('#list');
+        $('<li><input type="text" class="text" style="width:auto" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span><span class="completed">Done</span></li>').appendTo('#list');
         $('#input').val(null);
     });
     
     //Key pressed event for the enter key
     $('#input').bind("enterKey", function (e) {
-        $('<li><input type="text" class="text" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span></li>').appendTo('#list');
+        $('<li><input type="text" class="text" style="width:auto" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span></li>').appendTo('#list');
         $('#input').val(null);
     });
 
@@ -20,6 +20,12 @@ $(document).ready(function () {
     //Removes an item when the "X" is clicked
     $(document).on('click', '.remove', function () {
         $(this).parent().remove();
+    });
+  
+    //Removes an item when the "X" is clicked
+    $(document).on('click', '.completed', function () {
+        $(this).parent().remove();
+        // Do something extra for a completed task
     });
     
 });
