0e171154ef3f98b4c19a1912df68176c2c715cc5
GreatGeneral <hamzamohamed113@gmail.com>
2022-01-25 00:09:00 +0000

    Update script.js
    Fixed the enter key event to fit the input field format

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     
     //Key pressed event for the enter key
     $('#input').bind("enterKey", function (e) {
-        $('<li class="list-group-item"><span class="remove">X</span>' + $('#input').val() + '</li>').appendTo('#list');
+        $('<li><input type="text" class="text" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span></li>').appendTo('#list');
         $('#input').val(null);
     });
 
