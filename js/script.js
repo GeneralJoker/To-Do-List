fe9ecb0f5527b31f99dee2f7dfcf485b0576c09d
GreatGeneral <hamzamohamed113@gmail.com>
2022-01-26 23:48:47 +0000

    Added a completed function
    Text is struck through when task is completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //Click Event
     $('#addButton').click(function () {
-        $('<li><input type="text" class="text" style="width:auto" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span><span class="completed">Done</span></li>').appendTo('#list');
+        $('<li><input type="text" class="text" style="width:auto" value="' + $('#input').val() +'" readonly' + '/><span class="remove">X</span></li>').appendTo('#list');
         $('#input').val(null);
     });
     
@@ -22,10 +22,31 @@ $(document).ready(function () {
         $(this).parent().remove();
     });
   
-    //Removes an item when the "X" is clicked
-    $(document).on('click', '.completed', function () {
-        $(this).parent().remove();
+    //Strikes through an item when task is clicked
+    $(document).on('click', '.text', function () {
+        //$(this).parent().remove();
         // Do something extra for a completed task
+        //$(".text:active").css("text-decoration", "line-through");
+        $(".text:focus, .text:active").css("text-decoration", "line-through");
     });
-    
+  
+    // Validation for null tasks 
+    $(document).ready(function() {
+      $('.text').keyup(function() {
+
+      var empty = false;
+      $('.text').each(function() {
+          if ($(this).val().length == 0) {
+              empty = true;
+          }
+      });                   
+
+      if (empty) {
+          $('button').attr('disabled', 'disabled');
+      } else {
+          $('button').removeAttr('disabled');
+      }                
+      });
+    });   
+  
 });
